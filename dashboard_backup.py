85d2cad03e201c5bd9e65a99946e79870fa715f0
Piyush Thakare <Piyush.Thakare@sunpowercorp.com>
2022-04-19 22:31:11 +0530

    Added new code to take all dashboard id's and then to store each of the single dashboard id into a folder and then add that folder to s3.

--- dashboard_backup.py
+++ dashboard_backup.py
@@ -2,33 +2,60 @@ import os
 import env
 import json
 import boto3
+import shutil
 import datetime
 from datadog import initialize, api
 
-# TO GET CURRENT DATE
-currentDate = datetime.datetime.now()
-
-# FORMATTING THE DATE WITH FORMAT ```DAY-MONTH_NAME-YEAR```
-formattedDate = currentDate.strftime("%d-%B-%Y")
-
-# FILENAME WITH FORMAT ```CurrentDate.json```
-filename = formattedDate+".json"
-
-# DATADOG API-KEY and APP-KEY
+# DATADOG INITIALIZATION
 options = {
     'api_key': env.DATADOG_API_KEY,
     'app_key': env.DATADOG_APP_KEY
 }
 
-# PASSING THE ARGUMENTS
 initialize(**options)
 
-# API CALL TO GET ALL THE DASHBOARDS
-apiResponse = api.Dashboard.get_all()
+# API CALL TO GET ALL THE DASHBOARDS ID'S
+try:
+    apiResponse = api.Dashboard.get_all()
+    dashbaordIdsList = [element["id"]
+                        for element in apiResponse.get('dashboards')]
+except:
+    print("Error In Calling The API!!!")
 
-# CREATING A FILE TO STORE THE RESPONSE
-with open(filename, 'w') as convert_file:
-    convert_file.write(json.dumps(apiResponse))
+# FORMATTING THE DATE WITH FORMAT ```DAY-MONTH_NAME-YEAR```
+formattedDate = datetime.datetime.now().strftime("%d-%B-%Y")
+
+# CREATING A FOLDER WITH TODAY'S DATE
+workingDirectory = os.getcwd()  # gets the current working directory
+
+
+# here formattedDate is the name of the folder i.e: current date
+path = os.path.join(workingDirectory, formattedDate)
+
+if os.path.isdir(path):
+    shutil.rmtree(path)  # deletes the directory if already exsists
+
+os.mkdir(path)  # creates a directory
+
+for i in dashbaordIdsList:
+    try:
+        # API call to datadog for getting single dashboard data
+        dashboardData = api.Dashboard.get(i)
+        fileTitle = dashboardData.get('title')
+        formattedFileTitle = fileTitle.replace(" ", "")
+        file_name = formattedFileTitle+".json"
+
+        # Creating file with application name to store the json data
+        with open(file_name, 'w') as fp:
+            fp.write(json.dumps(dashboardData))
+            shutil.move(workingDirectory+"/"+file_name, path+"/"+file_name)
+            pass
+    except:
+        print("Error in calling the API!!!")
+
+shutil.make_archive(formattedDate, 'zip', path)  # Compressing the Folder
+
+fileToUpload = formattedDate+".zip"
 
 # AWS INITIALIZATION TO UPLOAD THE FILE TO S3 Bucket
 session = boto3.Session(
@@ -37,11 +64,16 @@ session = boto3.Session(
 )
 s3 = session.resource('s3')
 s3.meta.client.upload_file(
-    Filename=filename, Bucket=env.S3_BUCKET_NAME, Key=filename
+    Filename=fileToUpload, Bucket=env.S3_BUCKET_NAME, Key=fileToUpload
 )
 
 # DELETING THE CREATED FILE
-if os.path.exists(filename):
-    os.remove(filename)
+if os.path.exists(fileToUpload):
+    os.remove(fileToUpload)
+else:
+    print("The file does not exist!!!")
+
+if os.path.exists(path):
+    shutil. rmtree(path)
 else:
     print("The file does not exist!!!")
